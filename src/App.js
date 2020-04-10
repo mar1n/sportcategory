143b8728149181b35e4bdf4fe73c6d1d1cc1b46b
mar1n <30738216+mar1n@users.noreply.github.com>
2020-04-10 23:07:53 +0100

    display item in list css grid

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import Football from './football.jpg';
+import Basketball from './basketball.jpg';
+import Icehockey from './icehockey.jpg';
+import Tennis from './tennis.jpg';
+import Rugby from './rugby.jpg';
+import Vollyball from './vollyball.jpg';
 import './App.css';
 
 function App() {
@@ -8,6 +13,26 @@ function App() {
       <div className="sport">
         <h1>Football</h1>
         <img src={Football} alt="Football" />
+      </div>
+      <div className="sport">
+        <h1>Basketball</h1>
+        <img src={Basketball} alt="Basketball" />
+      </div>
+      <div className="sport">
+        <h1>Ice hockey</h1>
+        <img src={Icehockey} alt="Icehockey" />
+      </div>
+      <div className="sport">
+        <h1>Tennis</h1>
+        <img src={Tennis} alt="Tennis" />
+      </div>
+      <div className="sport">
+        <h1>Rugby</h1>
+        <img src={Rugby} alt="Rugby" />
+      </div>
+      <div className="sport">
+        <h1>Vollyball</h1>
+        <img src={Vollyball} alt="Vollyball" />
       </div>
     </div>
   );

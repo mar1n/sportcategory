b9bd561894a84876ad9a6e6971e812a02e3e69d7
mar1n <30738216+mar1n@users.noreply.github.com>
2020-04-14 23:35:26 +0100

    return data from backend

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const sportget = require('./SportGet');
 
-app.get('/', (req, res) => res.send('Hello Wolrd!'))
+app.get('/rest/sport', (req, res) => res.send(sportget))
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))

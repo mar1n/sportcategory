990085dad21d26a83e71d34ccc051ff4245a94b6
mar1n <30738216+mar1n@users.noreply.github.com>
2020-04-21 20:39:30 +0100

    connect db on cloud and on localhost

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,13 +1,14 @@
+require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
-const url = "mongodb://sportmen:sportmen@localhost:27017/sportcategory";
-const url2 = "mongodb+srv://sportmen:turboman54@cluster0-onxaf.mongodb.net/test?retryWrites=true&w=majority"
-const db2Name = "categorysport"
+const url2 = process.env.NODE_ENV === 'development' ? process.env.DB_URL_DEV : process.env.DB_URL_PRD;
+const dbName = process.env.NODE_ENV === 'development' ? 'sportcategory' : 'categorysport';
+
 function connect() {
     return new Promise((resolve, reject) => {
         MongoClient.connect(url2, { useUnifiedTopology: true, useNewUrlParser: true },  (error, db) => {
             if (error) reject(error);
-            const dbo = db.db('categorysport');
+            const dbo = db.db(dbName);
             console.log(dbo);
             resolve(dbo);
         });

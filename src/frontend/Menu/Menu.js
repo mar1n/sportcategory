d74b5abccaf5857b47cfd5cc7ccb537b3cfd8c6a
mar1n <30738216+mar1n@users.noreply.github.com>
2020-04-22 21:57:42 +0100

    menu slide in

--- src/frontend/Menu/Menu.js
+++ src/frontend/Menu/Menu.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Menu.css'
 import menuicon from '../../images/menu-icon.svg'
 
-const Menu = () => 
-    <div className='menu'>
-        <img className='icon' src={menuicon} alt='menu icon'></img>
-    </div>
+const Menu = () => {
+    const [visible, setVisible] = useState(false);
 
+    const handleClick = () => {
+        setVisible(!visible);
+    }
+    return (
+        <div className={`Menu ${visible ? 'is-visible' : ''}`}>
+            <button onClick={handleClick}>
+                <img className='icon' src={menuicon} alt='menu icon'></img>
+            </button>
+            <div className='panel'>
+                <div className='dimmer' onClick={handleClick} />
+                <div className='bar'><div className='title'>Hello menu!</div>
+                </div>
+            </div>
+        </div>
+    )
+}
 export default Menu;

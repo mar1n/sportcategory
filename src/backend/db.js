c6160183b702df29cff42ee2ab1852fa06a7d0a2
mar1n <30738216+mar1n@users.noreply.github.com>
2021-10-17 23:35:44 +0100

    db url

--- src/backend/db.js
+++ src/backend/db.js
@@ -4,8 +4,8 @@ const MongoStore = require('connect-mongo')(session)
 const DB_SESSION_SALT = 'keyboard cat'
 const MongoClient = require('mongodb').MongoClient
 
-const url2 = process.env.NODE_ENV === 'development' ? process.env.DB_URL_DEV : process.env.DB_URL_PRD
-const dbName = process.env.NODE_ENV === 'development' ? 'sportcategory' : 'categorysport'
+const url2 = 'mongodb+srv://sportmen:turboman54@cluster0-onxaf.mongodb.net/categorysport?retryWrites=true&w=majority'
+const dbName = 'categorysport'
 
 function connect() {
     return new Promise((resolve, reject) => {
